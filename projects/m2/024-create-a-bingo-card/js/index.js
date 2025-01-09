e7c92f59e84dfa6320b5e8199f5dfb0db6b10cd2
Benito Rubano <rubanobenito@gmail.com>
2025-01-09 11:10:45 +0100

    solution/M2-024

---
+++ projects/m2/024-create-a-bingo-card/js/index.js
@@ -0,0 +1,47 @@
+const bindDict={
+    'B':[1,15],
+    'I':[16,30],
+    'N':[31,45],
+    'G':[46,60],
+    'O':[61,75]
+}
+
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
+  }
+
+function getBingo(){
+    const bingo={};
+    const keys=Object.keys(bindDict);
+    for(let key of keys){
+        let[min,max]=bindDict[key];
+        min *= 1;
+        max *= 1;
+        const line=[];
+        while(line.length < 5){
+           console.log('here',line.length)
+            let random = getRandomIntInclusive(min,max);
+            if(!line.includes(random)){
+                line.push(random)
+            }
+        }
+        bingo[key] = line;
+    }
+
+    return bingo;
+}
+  
+
+
+function main(){
+    const bingoTry = getBingo();
+    console.log(Object.entries(bingoTry))
+    for(let [key,arr] of Object.entries(bingoTry)){
+        console.log(`${key} ${arr.join(' ')} \n`)
+    }
+}
+
+main()
