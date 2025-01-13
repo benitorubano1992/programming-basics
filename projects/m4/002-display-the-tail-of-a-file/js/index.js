6333b8ee71af5d4cf27210c0fe99e08716977ed6
Benito Rubano <rubanobenito@gmail.com>
2025-01-13 09:51:05 +0100

    solution/M4--002

--- projects/m4/002-display-the-tail-of-a-file/js/index.js
+++ projects/m4/002-display-the-tail-of-a-file/js/index.js
@@ -1,6 +1,10 @@
 const readline = require('readline/promises');
 const { stdin: input, stdout: output } = require('process');
-const fs = require('fs')
+const fs = require('node:fs');
+
+
+
+
 
 
 
@@ -11,10 +15,11 @@ async function main(){
         const nameFile = await getInput(rl);
         const path = `${__dirname}/../${nameFile}`
         
-        const arrString = await leggiFile(path)
+        const arrString = await leggiFile(path,rl);
         const fileArr = arrString.split('\n');
         const tail = fileArr.slice(fileArr.length - 10).join('\n')
         console.log(tail);
+        //console.log('fine',arrString.length);
 
 
 }
@@ -30,19 +35,29 @@ finally{
 main();
 
  function leggiFile(path){
-
     return new Promise((resolve,reject)=>{
-        fs.readFile(path,'utf-8',(err,data)=>{
-            if(err){
-                reject(err)
-            }
-            resolve(data)
-        })
+    
+    const rr = fs.createReadStream(path,'utf-8');
+    const arrResult=[]
 
-    })
+    rr.on('data', (data) => {
+    console.log({data},arrResult.length)
+    arrResult.push(data)
+});
+rr.on('end', () => {
+    console.log('end','finish rading the file');
+    resolve(arrResult)
+}); 
+rr.on('error',(e)=>{
+    reject(e)
+})
+    
+
+})
+}
 
 
-} 
+
 
 
 async function getInput(rl){
